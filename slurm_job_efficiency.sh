1c30611934d0d79adbbb67f90050c5417f44be81
malone12 <malone12@illinois.edu>
2021-10-22 21:29:58 -0500

    Handle multiple jobs for same user, group, and partition finishing at exact same time

--- slurm_job_efficiency.sh
+++ slurm_job_efficiency.sh
@@ -3,7 +3,7 @@
 tfile=$(mktemp /tmp/seff.XXXXXX)
 source /etc/telegraf/slurm_config
 
-jobs=($(mysql -u ${username} -p${password} -D ${database} -e "select id_job from ${job_table} where time_end > UNIX_TIMESTAMP(now() - interval 1 hour) and exit_code = '0' and array_task_pending = '0'" | grep -v id_job))
+jobs=($(mysql -u ${username} -p${password} -D ${database} -e "select id_job from ${job_table} where time_end > UNIX_TIMESTAMP(now() - interval 2 hour) and exit_code = '0' and array_task_pending = '0'" | grep -v id_job))
 
 for j in ${jobs[@]}
 do
@@ -14,7 +14,17 @@ do
 	mem_efficiency=$(grep "Memory Efficiency:" ${tfile} | cut -d' ' -f 3 | cut -d'%' -f 1)
 	partition=$(${slurm_path}/sacct -P -X -n -o partition%20 -j ${j})
 	end_stamp=$(${slurm_path}/sacct -P -X -n -o End -j ${j})
-	end_time=$(date -d "${end_stamp}" +%s%N | head -n 1)
+	new_end_time=$(date -d "${end_stamp}" +%s%N | head -n 1)
+	if [ -z ${end_time} ] || [ ${new_end_time} -ne ${end_time} ]; then
+		end_time="${new_end_time}"
+		real_end_time="${new_end_time}"
+		n=0
+	else
+		n=$((n+1))
+		mult=$((6000000*n))
+		end_time="${new_end_time}"
+		real_end_time=$((new_end_time+mult))
+	fi
 	raw_time=$(grep "CPU Efficiency:" ${tfile} | cut -d' ' -f 5)
 	if [ -z "$(echo ${raw_time} | grep "-")" ]; then
 		## Less than 1 day of core time
@@ -25,7 +35,7 @@ do
 		core_time=$((from_core_days+from_core_time))
 	fi
 
-	echo "slurm_job_efficiency,user=${user},group=${group},partition=${partition} cpu_efficiency=${cpu_efficiency},mem_efficiency=${mem_efficiency},core_time=${core_time} ${end_time}"
+	echo "slurm_job_efficiency,user=${user},group=${group},partition=${partition} cpu_efficiency=${cpu_efficiency},mem_efficiency=${mem_efficiency},core_time=${core_time} ${real_end_time}"
 done
 
 rm -rf ${tfile}
