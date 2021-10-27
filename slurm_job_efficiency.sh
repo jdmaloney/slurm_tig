dd119460c7cd00e7ec20f6dd4bb255f516837e28
malone12 <malone12@illinois.edu>
2021-10-27 16:27:52 -0500

    Caught a few conditions that would ingest useless data

--- slurm_job_efficiency.sh
+++ slurm_job_efficiency.sh
@@ -3,7 +3,7 @@
 tfile=$(mktemp /tmp/seff.XXXXXX)
 source /etc/telegraf/slurm_config
 
-jobs=($(mysql -u ${username} -p${password} -D ${database} -e "select id_job from ${job_table} where time_end > UNIX_TIMESTAMP(now() - interval 2 hour) and exit_code = '0' and array_task_pending = '0'" | grep -v id_job))
+jobs=($(mysql -u ${username} -p${password} -D ${database} -e "select id_job from ${job_table} where time_end > UNIX_TIMESTAMP(now() - interval 5 hour) and exit_code = '0' and array_task_pending = '0'" | grep -v id_job))
 
 for j in ${jobs[@]}
 do
@@ -14,28 +14,32 @@ do
 	mem_efficiency=$(grep "Memory Efficiency:" ${tfile} | cut -d' ' -f 3 | cut -d'%' -f 1)
 	partition=$(${slurm_path}/sacct -P -X -n -o partition%20 -j ${j} | head -n 1)
 	end_stamp=$(${slurm_path}/sacct -P -X -n -o End -j ${j} | sort -r | head -n 1)
-	new_end_time=$(date -d "${end_stamp}" +%s%N | head -n 1)
-	if [ -z ${end_time} ] || [ ${new_end_time} -ne ${end_time} ]; then
-		end_time="${new_end_time}"
-		real_end_time="${new_end_time}"
-		n=0
-	else
-		n=$((n+1))
-		mult=$((6000000*n))
-		end_time="${new_end_time}"
-		real_end_time=$((new_end_time+mult))
-	fi
-	raw_time=$(grep "CPU Efficiency:" ${tfile} | cut -d' ' -f 5)
-	if [ -z "$(echo ${raw_time} | grep "-")" ]; then
-		## Less than 1 day of core time
-		core_time=$(echo ${raw_time} | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
-	else
-		from_core_days=$(echo ${raw_time} | awk -F- '{print ($1 *86400) }')
-		from_core_time=$(echo ${raw_time} | cut -d'-' -f 2 | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
-		core_time=$((from_core_days+from_core_time))
-	fi
+	if [ ${end_stamp} != "Unknown" ]; then
+		new_end_time=$(date -d "${end_stamp}" +%s%N | head -n 1)
+		if [ -z ${end_time} ] || [ ${new_end_time} -ne ${end_time} ]; then
+			end_time="${new_end_time}"
+			real_end_time="${new_end_time}"
+			n=0
+		else
+			n=$((n+1))
+			mult=$((6000000*n))
+			end_time="${new_end_time}"
+			real_end_time=$((new_end_time+mult))
+		fi
+		raw_time=$(grep "CPU Efficiency:" ${tfile} | cut -d' ' -f 5)
+		if [ -z "$(echo ${raw_time} | grep "-")" ]; then
+			## Less than 1 day of core time
+			core_time=$(echo ${raw_time} | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+		else
+			from_core_days=$(echo ${raw_time} | awk -F- '{print ($1 *86400) }')
+			from_core_time=$(echo ${raw_time} | cut -d'-' -f 2 | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+			core_time=$((from_core_days+from_core_time))
+		fi
 
-	echo "slurm_job_efficiency,user=${user},group=${group},partition=${partition} cpu_efficiency=${cpu_efficiency},mem_efficiency=${mem_efficiency},core_time=${core_time} ${real_end_time}"
+		if [ "${core_time}" -ne 0 ]; then
+		echo "slurm_job_efficiency,user=${user},group=${group},partition=${partition} cpu_efficiency=${cpu_efficiency},mem_efficiency=${mem_efficiency},core_time=${core_time} ${real_end_time}"
+		fi
+	fi
 done
 
 rm -rf ${tfile}
