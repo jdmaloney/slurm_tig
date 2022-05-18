0661ce01994f2d53570d6ab4d2541f35e6a55358
malone12 <malone12@illinois.edu>
2022-05-18 08:35:23 -0500

    Beta Commit of Slurm Accounting Data

---
+++ slurm_accounting_stats.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+tfile=$(mktemp /tmp/sacct.XXXXXX)
+source /etc/telegraf/slurm/slurm_config
+
+if [ -z "$password" ]
+then
+  # ASSUME NO PASSWORD NECESSARY, e.g. USING SOCKET
+  mysqlpass=""
+else
+  mysqlpass="-p${password}"
+fi
+
+## Get data into temp files for parsing
+mysql -u ${username} ${mysqlpass} -D ${database} -e "select id_user,account,\`partition\`,tres_req,(time_end - time_start), time_end from ${job_table} where time_end != '0' and array_task_pending = '0'" | sed 's/\t/:/g' > ${tfile}
+mysql -u ${username} ${mysqlpass} -D ${database} -e "select id,type,\`name\` from tres_table where deleted = '0'" | sed 's/\t/:/g' > ${tfile}.tres
+
+old_job_end=""
+many_same=""
+while IFS= read -r line; do
+	resource_usage_string=""
+	IFS=":" read -r id_user account partition tres_raw job_time_seconds job_end_time <<< "${line}"
+
+	## Handle jobs that end at same exact second by incrementing their timestamp by 1 microsecond
+	njob_end_time="${job_end_time}000"
+	if [ ${old_job_end} -eq ${job_end_time} ]; then
+		if [ ${many_same} -eq 0 ]; then
+			add_num=1
+			many_same=1
+		else
+			add_num=$((many_same+1))
+			many_same=${add_num}
+		fi
+		njob_end_time=$((njob_end_time+add_num))
+	else
+		many_same=0
+	fi
+	old_job_end=${job_end_time}
+
+	## Walk through all tres/gres resource ids for tracking
+	ids=($(echo ${tres_raw} | sed 's/,/\n/g' | cut -d'=' -f 1 | xargs))
+	for i in ${ids[@]}
+	do
+		if [ -z $(awk -F : -v id=${i} '$1 == id {print $0}' ${tfile}.tres | grep gres) ]; then
+			## Not a GRES
+			field=$(awk -F : -v id=${i} '$1 == id {print $2}' ${tfile}.tres)
+			field_value=$(echo ${tres_raw} | sed 's/,/\n/g' | awk -F = -v id=${i} '$1 == id {print $2}')
+			field_seconds=$(echo "scale=5; ${field_value} * ${job_time_seconds}" | bc -l)
+			resource_usage_string="$(echo ${resource_usage_string}),${field}_seconds=${field_seconds}"
+		else
+			## Is a GRES
+			field=$(awk -F : -v id=${i} '$1 == id {print $3"_"$4}' ${tfile}.tres)
+                        field_value=$(echo ${tres_raw} | sed 's/,/\n/g' | awk -F = -v id=${i} '$1 == id {print $2}')
+                        field_seconds=$(echo "scale=5; ${field_value} * ${job_time_seconds}" | bc -l)
+                        resource_usage_string="$(echo ${resource_usage_string}),${field}_seconds=${field_seconds}"
+		fi
+	done
+
+	## Clean up the resource string and echo final output
+	resource_usage_string=$(echo ${resource_usage_string} | cut -d',' -f 2- | sed 's/__/_/g')
+	echo "slurm_job_accounting_data,partition=${partition},user=${id_user},account=${account} ${resource_usage_string} ${njob_end_time}"
+done < <(cat "${tfile}")
